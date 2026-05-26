e4536bcf04ddd372739d053bbd00bdfb79005601
JuanP <jpchalaramirez@gmail.com>
2026-05-26 17:22:15 -0500

    feat: frontend-web/src/App.tsx

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css';
 import MainLayout from './layouts/MainLayout'
+import Productos from './view/products/Products';
 
 function App(){
   return (
     <MainLayout>
         <h1>Dashboard</h1>
+
+        <Productos />
     </MainLayout>
   )
 }
