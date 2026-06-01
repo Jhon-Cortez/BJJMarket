3b20cdeb1819cadf0db6eaf97ec09a17b23be51e
Jhon-Cortez <cortezvargasjhon@gmail.com>
2026-06-01 09:23:57 -0500

    fix:fix name in entity role

--- backend/src/main/java/com/BJJMarket/backend/modules/auth/entity/Role.java
+++ backend/src/main/java/com/BJJMarket/backend/modules/auth/entity/Role.java
@@ -18,12 +18,12 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Role extends Auditable{
+public class Role{
     @Id
     @Column(name = "role_id")
     private Integer roleId;
 
-    @Column(name = "description", columnDefinition = "VARCHAR(60)", nullable = false, unique = true)
+    @Column(name = "name", columnDefinition = "VARCHAR(60)", nullable = false, unique = true)
     private String name;
 
     @Column(name = "description", columnDefinition = "VARCHAR(100)")
