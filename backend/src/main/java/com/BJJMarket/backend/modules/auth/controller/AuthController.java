a201198bdda914e0fcbfcb20a2b9832b3ebe4d90
Jhon-Cortez <cortezvargasjhon@gmail.com>
2026-06-01 07:21:04 -0500

    feat:Register enpoint created

---
+++ backend/src/main/java/com/BJJMarket/backend/modules/auth/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.BJJMarket.backend.modules.auth.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.BJJMarket.backend.modules.auth.dto.request.RegisterRequest;
+import com.BJJMarket.backend.modules.auth.service.AuthService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> Register(@RequestBody RegisterRequest request){
+        authService.register(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Usuario registrado correctamente");
+    }
+}
